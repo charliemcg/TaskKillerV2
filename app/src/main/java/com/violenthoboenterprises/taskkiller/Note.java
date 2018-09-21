213026fd2f284b611d72a753ab4e0d4bada29054
charliemcg <charlie_mcgregor@outlook.com>
2018-09-21 14:14:16 +0800

    Tidied note UI to look better without the add and edit buttons.

--- app/src/main/java/com/violenthoboenterprises/taskkiller/Note.java
+++ app/src/main/java/com/violenthoboenterprises/taskkiller/Note.java
@@ -26,9 +26,7 @@ public class Note extends MainActivity {
     TextView noteTextView;
     EditText noteEditText;
     InputMethodManager keyboard;
-    Button editBtn;
     Button removeBtn;
-    Button addNoteBtn;
     Button submitNoteBtn;
     String TAG;
     String theNote;
@@ -46,9 +44,7 @@ public class Note extends MainActivity {
         noteTextView = findViewById(R.id.noteTextView);
         noteEditText = findViewById(R.id.noteEditText);
         keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-//        editBtn = findViewById(R.id.editBtn);
         removeBtn = findViewById(R.id.removeBtn);
-        addNoteBtn = findViewById(R.id.addNoteBtn);
         submitNoteBtn = findViewById(R.id.submitNoteBtn);
         TAG = "Note";
         theNote = "";
@@ -57,15 +53,6 @@ public class Note extends MainActivity {
         noteRoot = findViewById(R.id.noteRoot);
 
         noteToolbar.setTitleTextColor(Color.parseColor(highlight));
-
-//        //getting task data
-//        String dbTask = "";
-//        Cursor dbTaskResult = MainActivity.db.getUniversalData();
-//        while (dbTaskResult.moveToNext()) {
-//            dbTask = dbTaskResult.getString(4);
-//        }
-//
-//        noteToolbar.setTitle(dbTask);
 
         //getting task data
         String dbTask = "";
@@ -81,8 +68,6 @@ public class Note extends MainActivity {
 
         noteToolbar.setTitle(dbTask);
 
-//        mute = mSharedPreferences.getBoolean("muteKey", false);
-
         noteTextView.setMovementMethod(new ScrollingMovementMethod());
 
         //getting app-wide data
@@ -94,9 +79,7 @@ public class Note extends MainActivity {
         dbResult.close();
 
         if(mute){
-//            editBtn.setSoundEffectsEnabled(false);
             removeBtn.setSoundEffectsEnabled(false);
-            addNoteBtn.setSoundEffectsEnabled(false);
             submitNoteBtn.setSoundEffectsEnabled(false);
         }
 
@@ -153,24 +136,16 @@ public class Note extends MainActivity {
                     //show remove button
                     removeBtn.setVisibility(View.VISIBLE);
 
-//                    editBtn.setText(R.string.edit);
-//                    editBtn.setVisibility(View.GONE);
-
                     //Hide text box
                     noteEditText.setVisibility(View.GONE);
 
                     //Hide submit button
                     submitNoteBtn.setVisibility(View.GONE);
 
-                }else{
-//                    editBtn.setText(R.string.addNote);
                 }
 
                 //Hide keyboard
                 keyboard.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-
-                //Show edit button
-//                editBtn.setVisibility(View.VISIBLE);
 
             }
 
@@ -180,10 +155,8 @@ public class Note extends MainActivity {
         noteTextView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-//                MainActivity.vibrate.vibrate(50);
 
-                //hide edit button
-//                editBtn.setVisibility(View.GONE);
+//                MainActivity.vibrate.vibrate(50);
 
                 //hide remove button
                 removeBtn.setVisibility(View.GONE);
@@ -212,42 +185,6 @@ public class Note extends MainActivity {
             }
         });
 
-        //Actions to occur if user selects 'Edit'
-//        editBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-////                MainActivity.vibrate.vibrate(50);
-//
-//                //hide edit button
-//                editBtn.setVisibility(View.GONE);
-//
-//                //hide remove button
-//                removeBtn.setVisibility(View.GONE);
-//
-//                //show edit text
-//                noteEditText.setVisibility(View.VISIBLE);
-//
-//                //show submit button
-//                submitNoteBtn.setVisibility(View.VISIBLE);
-//
-//                //Focus on edit text so that keyboard does not cover it up
-//                noteEditText.requestFocus();
-//
-//                //show keyboard
-//                keyboard.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-//
-//                //set text to existing note
-//                noteEditText.setText(theNote);
-//
-//                //put cursor at end of text
-//                noteEditText.setSelection(noteEditText.getText().length());
-//
-//                noteTextView.setText("");
-//
-//            }
-//        });
-
         //Actions to occur if user selects 'Remove'
         removeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -268,30 +205,12 @@ public class Note extends MainActivity {
 
                 noteTextView.setText("");
 
-                //hide edit button
-//                editBtn.setVisibility(View.GONE);
-
                 //hide remove button
                 removeBtn.setVisibility(View.GONE);
 
                 //show add button
-//                addNoteBtn.setVisibility(View.VISIBLE);
                 noteEditText.setVisibility(View.VISIBLE);
                 submitNoteBtn.setVisibility(View.VISIBLE);
-
-            }
-        });
-
-        //actions to occur if user selects 'add'
-        addNoteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                noteEditText.setVisibility(View.VISIBLE);
-                submitNoteBtn.setVisibility(View.VISIBLE);
-                noteEditText.requestFocus();
-                keyboard.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-                addNoteBtn.setVisibility(View.GONE);
 
             }
         });
@@ -328,8 +247,6 @@ public class Note extends MainActivity {
         }
         result.close();
 
-//        mute = mSharedPreferences.getBoolean("muteKey", false);
-
         Cursor dbResult = MainActivity.db.getUniversalData();
         while (dbResult.moveToNext()) {
             mute = dbResult.getInt(1) > 0;
@@ -345,7 +262,6 @@ public class Note extends MainActivity {
                     .LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
             noteEditText.setVisibility(View.GONE);
             submitNoteBtn.setVisibility(View.GONE);
-//            editBtn.setVisibility(View.GONE);
             removeBtn.setVisibility(View.VISIBLE);
 
         }
