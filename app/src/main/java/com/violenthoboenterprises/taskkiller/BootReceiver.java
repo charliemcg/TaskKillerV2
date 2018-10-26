890f17391e5bf5a3e79571f508a29be0a4684b57
charliemcg <charlie_mcgregor@outlook.com>
2018-10-26 21:47:50 +0800

    Implemented code for detecting a reboot.

---
+++ app/src/main/java/com/violenthoboenterprises/taskkiller/BootReceiver.java
@@ -0,0 +1,18 @@
+package com.violenthoboenterprises.taskkiller;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    String TAG = "BootReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Log.i(TAG, "Booted");
+        }
+    }
+}
